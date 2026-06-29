a419c82d614b3cc22b914a84e282b31b226bd0a0
elliot <elliotzen256@gmail.com>
2026-06-29 12:08:15 +0800

    adapte wsl

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,21 @@ require "nvchad.options"
 
 -- add yours here!
 
+if vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = {
+        name = "win32yank",
+        copy = {
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
+        },
+        cache_enabled = 0,
+    }
+end
+
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.clipboard = "unnamedplus"
