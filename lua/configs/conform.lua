ac9e6b95065e15d6d4c7829366728ee97dd698eb
elliot <elliotzen256@gmail.com>
2026-08-01 17:32:22 +0800

    feat: add TS/JS lsp & biome, switch theme to onedark
    Enable tsgo and biome LSPs, format JS/TS with biome, and bump plugin locks.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,7 +1,8 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
+    typescript = { "biome" },
+    javascript = { "biome" },
     -- html = { "prettier" },
   },
 
